09623486da5bc7f272a869a5023fbdf2c2694748
durgaprasad garapati <kdurgaprasadgarapati@gmail.com>
2023-01-11 16:52:21 +0530

    Encrypting password and saving in Db

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication5.Data;
+using EmployeeManagementAPI.Helper;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace EmployeeManagementAPI.Controllers
 {
@@ -19,11 +22,11 @@ namespace EmployeeManagementAPI.Controllers
         [HttpPost("Authenticate")]
         public async Task<IActionResult> AuthenticateUser([FromBody] Register register)
         {
+            
             if(register == null)
             {
                 return BadRequest();
             }
-
             var user = await dBContext.Registers.FirstOrDefaultAsync(x => x.UserName == register.UserName && x.Password == register.Password);
             if(user == null)
             {
@@ -37,13 +40,32 @@ namespace EmployeeManagementAPI.Controllers
         {
             try
             {
+                if (await CheckingExistingEmail(register.Email))
+                {
+                    return BadRequest(new { Message = "EmaiId Already Registered" });
+                }
+               
+                if(await CheckingUserName(register.UserName))
+                {
+                    return BadRequest(new { message = "Username already exist!" });
+                }
+                
+                var pass = CheckPasswordStrength(register.Password);
+                if(!string.IsNullOrEmpty(pass))
+                {
+                    return BadRequest(new { message = pass.ToString() });
+                }
+
                 if (register == null)
                 {
                     return BadRequest();
                 }
                 else
                 {
-                    var reg = await dBContext.Registers.AddAsync(register);
+                    register.Password = Helper.HashingPassword.HashPassword(register.Password);
+                    register.Role = "user";
+                    register.Token = "";
+                    await dBContext.Registers.AddAsync(register);
                     await dBContext.SaveChangesAsync();
                     return Ok(new { message = "Registered Sucessfully" });
                 }
@@ -54,6 +76,34 @@ namespace EmployeeManagementAPI.Controllers
                 var msg = ex.Message;
             }
             return Ok();
+        }
+
+        private async Task<bool> CheckingExistingEmail(string emailId)
+        {
+            return await dBContext.Registers.AnyAsync(x => x.Email.Equals(emailId));
+        }
+
+        private async Task<bool> CheckingUserName(string userName)
+        {
+            return await dBContext.Registers.AnyAsync(x => x.UserName.Equals(userName));
+        }
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(password.Length < 8)
+            {
+                sb.Append("Password length should be minimum 8 characters" + Environment.NewLine);
+            }
+            if(!(Regex.IsMatch(password,"[a-z]") && !Regex.IsMatch(password,"[A-Z]") && !Regex.IsMatch(password,"[0-9]")))
+            {
+                sb.Append("Password should be alphanumeric" + Environment.NewLine);
+            }
+            if (!(Regex.IsMatch(password, "[<,>,@,#,$,%,*,^,:,;,{,},[,],+,=]")))
+            {
+                sb.Append("Password should contain special characters" + Environment.NewLine);
+            }
+            return sb.ToString();
         }
     }
 }
---
+++ Helper/HashingPassword.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+namespace EmployeeManagementAPI.Helper
+{
+    public class HashingPassword
+    {
+        private static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        private static readonly int saltSize = 16;
+        private static readonly int hashSize = 20;
+        private static readonly int iterations = 10000;
+
+        public static string HashPassword(string password)
+        {
+            byte[] salt; 
+            rng.GetBytes(salt = new byte[saltSize]);
+            var key = new Rfc2898DeriveBytes(password, salt, iterations);
+            var hash = key.GetBytes(hashSize);
+            var hashBytes = new byte[saltSize + hashSize];
+            Array.Copy(salt, 0, hashBytes, 0, saltSize);
+            Array.Copy(hash, 0, hashBytes, saltSize, hashSize);
+            var base64HashPassword = Convert.ToBase64String(hashBytes);
+            return base64HashPassword;
+        }
+
+        public static bool VerifyPassword(string password, string base64Hash)
+        {
+            var hashBytes = Convert.FromBase64String(base64Hash);
+            var salt = new byte[saltSize];
+            Array.Copy(hashBytes,0,salt,0, saltSize);
+            var key = new Rfc2898DeriveBytes(password, salt, iterations);
+            byte[] hash = key.GetBytes(hashSize);
+            for (int i = 0; i < hashSize; i++)
+            {
+                if(hashBytes[i + saltSize] != hash[i])
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
